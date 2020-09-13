e46ba9c3ea3198df025761b0adfdfadbea038d2b
Kevin Huang <khuang159@gmail.com>
2020-09-13 21:49:00 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,22 +1,22 @@
-# def find_element_index(array, value_to_find)
-  
-# i = 0
- 
-# while i < array.length
-# do
-#   if array[i] == value_to_find
-#     return i
-#   end
-#   i += 1
-# end
-# end
-
 def find_element_index(array, value_to_find)
-  count = 0
-  while count < array.length do
-    if array[count] == value_to_find
-      return count
-    end
-    count += 1
+  
+i = 0
+ 
+while i < array.length
+do
+  if array[i] == value_to_find
+    return i
   end
+  i += 1
 end
+end
+
+# def find_element_index(array, value_to_find)
+#   count = 0
+#   while count < array.length do
+#     if array[count] == value_to_find
+#       return count
+#     end
+#     count += 1
+#   end
+# end

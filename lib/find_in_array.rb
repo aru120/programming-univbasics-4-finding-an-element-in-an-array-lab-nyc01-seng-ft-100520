3dba0fbdc60fd97e8693d957610fa38f90dedf5a
Kevin Huang <khuang159@gmail.com>
2020-09-13 21:48:08 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -10,3 +10,13 @@
 #   i += 1
 # end
 # end
+
+def find_element_index(array, value_to_find)
+  count = 0
+  while count < array.length do
+    if array[count] == value_to_find
+      return count
+    end
+    count += 1
+  end
+end
